c4c9a729b78374124fa3c1e0c772aaedf36982aa
Philipp-Hoss <92hoph1bif@hft-stuttgart.de>
2021-01-28 20:57:39 +0100

    meine Task06

---
+++ Task_06Philipp/src/de/hft_stuttgart/ip1/HuffmanDistribution.java
@@ -0,0 +1,7 @@
+package de.hft_stuttgart.ip1;
+
+public class HuffmanDistribution {
+    public static int[] getDistribution(byte[] input) {
+        throw new UnsupportedOperationException("This is your homework");
+    }
+}
