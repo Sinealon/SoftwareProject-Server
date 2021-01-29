a3a1c6ce5bc85990f1cd07ce7888acb3ea6f5115
C:\Users\Luca\Desktop\Software-Projekt-Server <luca.stanullo@gmail.com>
2021-01-29 14:18:20 +0100

    huffmen fix

--- Task_08_Ves3/src/Client/HuffmanTree.java
+++ Task_08_Ves3/src/Client/HuffmanTree.java
@@ -2,129 +2,77 @@ package Client;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-
 public class HuffmanTree {
-
-
     int [] distribution;
     HuffmanEntry entry;
 
-    public HuffmanTree(int[] distribution)
-    {
-        this.distribution = distribution;
-
-        List<HuffmanEntry> inputList = new ArrayList<>();
-
-        for (int i = 0; i < distribution.length; i++) {
-            inputList.add(new HuffmanEntry((byte) i, distribution[i]));
-        }
-
-        entry = CreateTree(inputList);
-    }
-
-    public HuffmanEntry CreateTree(List<HuffmanEntry> inputList)
-    {
-        while(inputList.size() > 1){
-            inputList.add(new HuffmanEntry(inputList.get(0), inputList.get(1)));
-            inputList.remove(0);
-            inputList.remove(0);
-            Collections.sort(inputList);
-        }
-
-        return inputList.get(0);
-    }
-    public byte[] encode(byte [] input)
-    {
-        String bytes = "";
-
-        for(byte b : input) {
-            HuffmanEntry current = entry;
-
-            while(true){
-                if (current.data.length <= 1) {
-                    break;
-                }
-
-                if (Contains(current.left.data, b)) {
-                    current = current.left;
-                    bytes += 1;
-                }
-
-                else {
-                    current = current.right;
-                    bytes += 0;
-                }
+    public HuffmanTree(int[] distribution) {
+            LinkedList<HuffmanEntry> liste = new LinkedList<>();
+            this.distribution = distribution;
+            for (int i = 0;i<distribution.length;i++) {
+            liste.add(new HuffmanEntry(Integer.valueOf(i).byteValue(),distribution[i]));
             }
-        }
-
-        bytes += 1;
-
-        while(bytes.length()%8 != 0) {
-            bytes += 0;
-        }
-
-        byte[] result = new byte[bytes.length()/8];
-
-        for (int i= 0; i < bytes.length(); i += 8) {
-            result[i/8] = (byte)Integer.parseInt(bytes.substring(i, i + 8), 2);
-        }
-
-        return result;
-    }
-
-    private boolean Contains(byte[] array, byte target)
-    {
-        for(int i : array) {
-            if(i == target) {
-                return true;
+            while (liste.size()>1) {
+            liste.sort(HuffmanEntry::compareTo);
+            HuffmanEntry links = liste.getFirst();
+            liste.removeFirst();
+            HuffmanEntry right = liste.getFirst();
+            liste.removeFirst();
+            liste.add(new HuffmanEntry(right,links));
             }
+            entry = liste.getFirst();
         }
 
-        return false;
-    }
-
-    public byte[] decode(byte[] input)
-    {
-        String bytes = "";
-
-        for(int i = 0; i < input.length*8; i++) {
-            bytes += get(input, i)?'1':'0';
-        }
-
-        HuffmanEntry current = entry;
-        List<Byte> result = new ArrayList<>();
-        for(char c : bytes.toCharArray()){
-
-            if(c == '1'){
-                current = current.left;
+    public byte[] encode(byte [] input) {
+            HuffmanEntry position = entry;
+            LinkedList<Boolean> liste = new LinkedList<>();
+            for (byte in:input) {
+            while(position.data.length>1){
+            liste.add((position.isLeft(in))? false:true);
+            position = (liste.getLast())? position.right:position.left;
             }
-            else{
-                current = current.right;
+            position = entry;
             }
-
-            if(current.data.length <= 1){
-                result.add(current.data[0]);
-                current = entry;
+            byte [] ergebniss = new byte[(liste.size()/8)+1];
+            for(int i = 0;i<liste.size();i++){
+            set(ergebniss,i,liste.get(i));
             }
+            set(ergebniss,liste.size(),true);
+            return ergebniss;
         }
 
-        byte[] realResult = new byte[result.size()];
-        for(int i = 0; i < realResult.length; i++){
-            realResult[i] = result.get(i);
+    public byte[] decode(byte[] input) {
+            HuffmanEntry position = entry;
+            LinkedList <Byte> ergebnisse = new  LinkedList();
+            LinkedList<Boolean> liste = new LinkedList<>();
+            for(int i = 0;i<input.length*8;i++){
+            liste.add(get(input,i));
+            }
+            for(int i = 0;i<liste.size();i++){
+            position = (liste.get(i))? position.right:position.left;
+            if(position.data.length == 1){
+            ergebnisse.add(position.data[0]);
+            position = entry;
+            }
+            }
+            byte [] ergebnissArray = new byte[ergebnisse.size()];
+            for(int i = 0; i<ergebnissArray.length;i++){
+            ergebnissArray[i] = ergebnisse.get(i);
+            }
+            return ergebnissArray;
         }
 
-        return realResult;
-    }
+    static private boolean get(byte [] buffer, int index) {
+            int b = index >> 3;
+            int m = 128 >> index%8;
+            return (buffer[b]&m)!=0;
+        }
 
-
-
-
-
-
-        static private boolean get(byte [] buffer, int index) {
-        int b = index >> 3;
-        int n = 128 >> index % 8;
-        return (buffer[b] & n) !=0;
-    }
+    static private void set(byte [] buffer, int index, boolean value) {
+            int b = index >> 3;
+            int m = 128 >> index % 8;
+            buffer[b] =(byte) (value? buffer[b]|m: buffer[b]&~m);
+        }
 }
